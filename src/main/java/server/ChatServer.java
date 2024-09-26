3453cc1ff19fe35fe074cb64b55f55dc0513c575
GlebPapulovsky <papulovskijglech@gmail.com>
2024-09-26 21:28:17 +0300

    test my ip

--- src/main/java/server/ChatServer.java
+++ src/main/java/server/ChatServer.java
@@ -8,7 +8,7 @@ public class ChatServer {
 
     public static void main(String[] args) throws IOException {
         //InetAddress locIP=InetAddress.getByName("188.170.82.204");
-        ServerSocket serverSocket = new ServerSocket(8080);
+        ServerSocket serverSocket = new ServerSocket(8090,40,InetAddress.getByName("192.168.8.100"));
         
         System.out.println("Server started. Waiting for clients...");
 
